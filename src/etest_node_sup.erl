79192e58945193ed96cd957735f9d9b3a7684a94
Dustin Sallings <dustin@spy.net>
2009-12-23 09:24:05 -0800

    Don't bother using a supervisor on the end node.

--- src/etest_node_sup.erl
+++ src/etest_node_sup.erl
@@ -1,24 +1,15 @@
 -module(etest_node_sup).
 
--behaviour(supervisor).
-
 -export([start_link/4]).
 
--export([init/1]).
+-export([init/4]).
 
 -define(SERVER, ?MODULE).
 
 start_link(Mod, Func, Args, Count) ->
-    supervisor:start_link({local, ?SERVER}, ?MODULE, {Mod, Func, Args, Count}).
+    spawn_link(?MODULE, init, [Mod, Func, Args, Count]).
 
-init({Mod, Func, Args, Count}) ->
-    Children = children(Mod, Func, Args, Count),
-    error_logger:info_msg("Starting children:  ~p~n", [Children]),
-    {ok, {{one_for_all,5,10}, Children}}.
-
-children(Mod, Func, Args, Count) ->
-    lists:map(fun (N) ->
-                      ChildName = list_to_atom(lists:concat(["worker", N])),
-                      {ChildName, {Mod, Func, Args},
-                       transient, 2000, worker, [Mod]}
+init(Mod, Func, Args, Count) ->
+    lists:map(fun(_N) ->
+                      spawn_link(Mod, Func, Args)
               end, lists:seq(1, Count)).
